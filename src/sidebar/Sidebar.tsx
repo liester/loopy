d2f29fb397b077de2cb17a4b6fd1b6caddf1bf46
Levi Liester <levi.liester@gmail.com>
2020-05-16 00:56:19 -0500

    Added friends

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Sidebar.scss'
+const friends: Array<string> = ['Lauren Beard', 'Esme Sanford', 'Leyla Smith','Shane Fisher','Daisy Bright','Talia Hodge','Carla Hart','Angela Cardenas']
 function Sidebar() {
     return (
         <div className="Sidebar">
-            Sidebar
+            {friends.map(friend => {
+                return (
+                    <div className={'contact'}>
+                        {friend}
+                    </div>
+                )
+            })}
         </div>
     );
 }
